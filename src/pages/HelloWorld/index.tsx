ea11f9e0e7a7bd1a53eb5d226c4773b1a4d92674
BrutLei <hoangphat4510@gmail.com>
2024-05-10 17:27:28 +0700

    handle typin route

---
+++ src/pages/HelloWorld/index.tsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+  return (
+    <div>
+      <p className="text-3xl">Hello People</p>
+    </div>
+  );
+};
+
+export default Hello;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
-import { LoginPage } from "../pages";
+import { HelloPage, LoginPage } from "../pages";
 
 export const routes = [
   {
     path: "/login",
     page: LoginPage,
+    layout: null,
+  },
+  {
+    path: "*",
+    page: HelloPage,
     layout: null,
   },
 ];
